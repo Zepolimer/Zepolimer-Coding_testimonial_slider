cd41fa1de3279dfd4fe4d42ba2c5640f0aa43cfa
Rémi Lopez <76875802+Zepolimer@users.noreply.github.com>
2021-06-17 10:16:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const btnPrev = document.querySelector('#switch1');
 const btnNext = document.querySelector('#switch2');
+const btnPrevbis = document.querySelector('#switch1bis');
+const btnNextbis = document.querySelector('#switch2bis');
 const tanya = document.querySelector('.quote-container1');
 const john = document.querySelector('.quote-container2');
 
@@ -12,6 +14,17 @@ function togg() {
         tanya.classList.add('display-none');
     }
 }
+function toggl() {
+    if (john.classList.contains('display-none')) {
+        john.classList.remove('display-none');
+        tanya.classList.add('display-none');
+    } else {
+        tanya.classList.remove('display-none');
+        john.classList.add('display-none');
+    }
+}
 
 btnPrev.addEventListener('click', togg);
 btnNext.addEventListener('click', togg);
+btnPrevbis.addEventListener('click', toggl);
+btnNextbis.addEventListener('click', toggl);
